bacf54ad63d58bdbc8639ce57c23ac4ce9d3a544
jatolentino <jose_antoniotv@hotmail.com>
2022-06-15 12:49:26 -0500

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(home: Text('Hello'),);
-	}
-}
